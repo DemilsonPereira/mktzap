183f480f1444ea30f0eab78059b21dc309b79e82
demilsonpereira <demilson.d2102@gmail.com>
2021-08-20 17:25:53 -0400

    feito a configuração

--- src/bkpwireicom/bkp.ts
+++ src/bkpwireicom/bkp.ts
@@ -4,39 +4,48 @@ import {
     Response
 } from 'express';
 
+import fs from 'fs';
+import path from 'path';
+
+
 class HistoryMensages {
-
     async serviceHistory(req: Request, res: Response) {
-        for (var page = 1; page <= 469; page++) {
-            try {
-                const response = await axios.get(
-                    `${process.env.BKP_URL}/${process.env.COMPANY_ID}/history`, {
-                    headers: {
-                        Authorization: `Bearer ${process.env.TOKEN}`,
-                        'x-page': page
-                    }
-                })
+        const file = __dirname + '/teste.json';
+        const ids = [];
+        for (var page = 1; page <= 1; page++) { //469
+            const response = await axios.get(
+                `${process.env.BKP_URL}/${process.env.COMPANY_ID}/history`, {
+                headers: {
+                    Authorization: `Bearer ${process.env.TOKEN}`,
+                    'x-page': page
+                }
+            })
+            const data = await response.data;
 
-                console.log(page)
+            // Salvar arquivo sem sobrescrever
+            fs.appendFile(file, JSON.stringify(data), err => {
+                console.log(err || 'Arquivo salvo');
+            });
+            // console.log(data)
 
-                const data = await response.data;
-
-                console.log(data.length);
-
-                const id = [...data].map(({ id }) => id);
-
-                console.log(id)
-
-                return res.json(id);
-            } catch (error) {
-                return res.status(400).json({ error: error.message });
-            }
+            // ids.push(data)
+            console.log(ids.push(data))
+            console.log(data.length);
         }
+
+        return ids
+        // return ids.flat(Infinity)
     }
+
+    // async lerJson(req: Request, res: Response) {
+    //     let data = fs.readFileSync(file_path + file_name);
+    //     data = JSON.parse(data);
+    //     console.log(data);
+    // }
 
     async messageHistory(req: Request, res: Response) {
         const response = await axios.get(
-            `${process.env.BKP_URL}/${process.env.COMPANY_ID}/history/128391113/message`, {
+            `${process.env.BKP_URL}/${process.env.COMPANY_ID}/history/66733102/message`, {
             headers: {
                 Authorization: `Bearer ${process.env.TOKEN}`,
                 'x-page': '1'
@@ -48,6 +57,8 @@ class HistoryMensages {
         return res.json(data);
     }
 }
+
+// copyFile('source.txt', 'destination.txt', HistoryMensages.toString);
 
 export {
     HistoryMensages
