b82c84d9c7f72d15d10f79997c41c2921af5ab52
demilsonpereira <demilson.d2102@gmail.com>
2021-08-24 13:36:10 -0400

    Refatoramento class HistoryMensages

--- src/bkpwireicom/bkp.ts
+++ src/bkpwireicom/bkp.ts
@@ -5,12 +5,11 @@ import {
 } from 'express';
 
 import fs from 'fs';
-import path from 'path';
-
-
 class HistoryMensages {
+    public response = []
+
     async serviceHistory(req: Request, res: Response) {
-        const file = __dirname + '/teste.json';
+        const file = __dirname + '/serviceHistory.json';
         const ids = [];
         for (var page = 1; page <= 1; page++) { //469
             const response = await axios.get(
@@ -23,29 +22,41 @@ class HistoryMensages {
             const data = await response.data;
 
             // Salvar arquivo sem sobrescrever
-            fs.appendFile(file, JSON.stringify(data), err => {
-                console.log(err || 'Arquivo salvo');
-            });
+            // fs.appendFile(file, JSON.stringify(data), err => {
+            //     console.log(err || 'Arquivo salvo');
+            // });
             // console.log(data)
 
-            // ids.push(data)
-            console.log(ids.push(data))
-            console.log(data.length);
+            ids.push(data)
+            // console.log(ids.push(data))
+            // console.log(data.length);
         }
 
-        return ids
+        return res.json(ids)
         // return ids.flat(Infinity)
     }
 
-    // async lerJson(req: Request, res: Response) {
-    //     let data = fs.readFileSync(file_path + file_name);
-    //     data = JSON.parse(data);
-    //     console.log(data);
+    // async messageHistoryModel() {
+    //     let contatMessage = require('./serviceHistory.json');
+    //     let { id } = contatMessage[0]
+    //     const response = await axios.get(
+    //         `${process.env.BKP_URL}/${process.env.COMPANY_ID}/history/${id}/message`, {
+    //         headers: {
+    //             Authorization: `Bearer ${process.env.TOKEN}`,
+    //             'x-page': '1'
+    //         }
+    //     })
+
+    //     const data = await response.data;
+
+    //     return data;
     // }
 
     async messageHistory(req: Request, res: Response) {
+        let contatMessage = require('./serviceHistory.json');
+        let { id } = contatMessage[0]
         const response = await axios.get(
-            `${process.env.BKP_URL}/${process.env.COMPANY_ID}/history/66733102/message`, {
+            `${process.env.BKP_URL}/${process.env.COMPANY_ID}/history/${id}/message`, {
             headers: {
                 Authorization: `Bearer ${process.env.TOKEN}`,
                 'x-page': '1'
@@ -56,9 +67,26 @@ class HistoryMensages {
 
         return res.json(data);
     }
-}
 
-// copyFile('source.txt', 'destination.txt', HistoryMensages.toString);
+    async customerHistory(req: Request, res: Response) {
+        let contatMessage = require('./serviceHistory.json');
+        // console.log(contatMessage);
+
+        let { id, name, created_at, contact_id, channelable_type, protocol } = contatMessage[0]
+
+        const data = { id, name, created_at, contact_id, channelable_type, protocol }
+
+        console.log(this.messageHistory)
+
+        // console.log({ data })
+
+        // const data = await response.data;
+
+        // return res.json(data);
+
+        return res.json(data)
+    }
+}
 
 export {
     HistoryMensages
