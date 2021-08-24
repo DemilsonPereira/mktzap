a626335ca7c6d2a740e4e6fbb29b0381c5691328
demilsonpereira <demilson.d2102@gmail.com>
2021-08-24 14:50:10 -0400

    Done

--- src/bkpwireicom/bkp.ts
+++ src/bkpwireicom/bkp.ts
@@ -37,8 +37,8 @@ class HistoryMensages {
     }
 
     // async messageHistoryModel() {
-    //     let contatMessage = require('./serviceHistory.json');
-    //     let { id } = contatMessage[0]
+    //     let contactMessage = require('./serviceHistory.json');
+    //     let { id } = contactMessage[0]
     //     const response = await axios.get(
     //         `${process.env.BKP_URL}/${process.env.COMPANY_ID}/history/${id}/message`, {
     //         headers: {
@@ -53,8 +53,8 @@ class HistoryMensages {
     // }
 
     async messageHistory(req: Request, res: Response) {
-        let contatMessage = require('./serviceHistory.json');
-        let { id } = contatMessage[0]
+        let contactMessage = require('./serviceHistory.json');
+        let { id } = contactMessage[0]
         const response = await axios.get(
             `${process.env.BKP_URL}/${process.env.COMPANY_ID}/history/${id}/message`, {
             headers: {
@@ -69,12 +69,45 @@ class HistoryMensages {
     }
 
     async customerHistory(req: Request, res: Response) {
-        let contatMessage = require('./serviceHistory.json');
-        // console.log(contatMessage);
+        let contactMessage = require('./serviceHistory.json');
+        // console.log(contactMessage);
 
-        let { id, name, created_at, contact_id, channelable_type, protocol } = contatMessage[0]
+        let { id, name, created_at, contact_id, channelable_type, protocol } = contactMessage[0]
 
-        const data = { id, name, created_at, contact_id, channelable_type, protocol }
+        const data = { id, name, created_at, contact_id, channelable_type, protocol, message: [
+            {
+                "id": 316511925,
+                "history_id": 24824129,
+                "user_id": null,
+                "sent_by_operator": 0,
+                "received_at": "2018-08-22T18:52:52.000Z",
+                "message": "\"Pode me enviar o meu boleto atualizado?\"",
+                "created_at": "2018-08-22T18:52:54.000Z",
+                "updated_at": "2018-08-22T18:52:54.000Z",
+                "sector_id": 6169,
+                "is_json": 1,
+                "metadata": null,
+                "company_id": 874,
+                "trigger_id": 27406,
+                "is_auto": 0
+              },
+              {
+                "id": 316511940,
+                "history_id": 24824129,
+                "user_id": null,
+                "sent_by_operator": 1,
+                "received_at": "2018-08-22T18:52:54.000Z",
+                "message": "\"\"",
+                "created_at": "2018-08-22T18:52:54.000Z",
+                "updated_at": "2018-08-22T18:52:54.000Z",
+                "sector_id": 6169,
+                "is_json": 1,
+                "metadata": null,
+                "company_id": 874,
+                "trigger_id": null,
+                "is_auto": 0
+              },
+        ] }
 
         console.log(this.messageHistory)
 
@@ -83,6 +116,8 @@ class HistoryMensages {
         // const data = await response.data;
 
         // return res.json(data);
+
+        Object.assign(data)
 
         return res.json(data)
     }
--- src/routes.ts
+++ src/routes.ts
@@ -13,6 +13,7 @@ const tokenAcess = new TokenAcess()
 
 router.get('/history/atendimento', historyMensages.serviceHistory);
 router.get('/history/message', historyMensages.messageHistory);
+router.get('/history/customer', historyMensages.customerHistory);
 router.get('/token', tokenAcess.token);
 
 
